bd7cd3caf862044088aa2b01f41d89ddf2b99e3b
Prasidh Gopal Anchan <prasidhgopalanchan@gmail.com>
2024-10-24 20:48:23 +0530

    chore: bump version to 2.0(2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.pika.pokedex"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
